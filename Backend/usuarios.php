bc1f2ba729bf82813515268a60793d1f1f143182
MarioDMC <MarioDMC@users.noreply.github.com>
2019-02-22 09:08:32 -0500

    Backend Insertar Usuarios
    JQuery para insertar usuarios

--- Backend/usuarios.php
+++ Backend/usuarios.php
@@ -10,23 +10,6 @@
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
     <!-- Custom styles for this template -->
     <link href="../css/style.css" rel="stylesheet">
   </head>
@@ -118,24 +101,19 @@
         </ul>
       </div>
     </nav>
-
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4" id="main">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+            <button type="button" class="btn btn-sm btn-outline-danger cancelar">Cancelar</button>
+            <button type="button" class="btn btn-sm btn-outline-success" id="nuevo_registro" >Nuevo</button>
           </div>
-          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            <span data-feather="calendar"></span>
-            This week
-          </button>
         </div>
       </div>
 
       <h2>Usuarios</h2>
-      <div class="table-responsive">
+      <div class="table-responsive view" id="show_data">
         <table class="table table-striped table-sm" id="list_usuarios">
           <thead>
             <tr>
@@ -149,6 +127,37 @@
           </tbody>
         </table>
       </div>
+       <div id="insert_data" class="view">
+       <form action="#" id="form_data">
+  <div class="row">
+  <div class="col">
+       <div class="form-group">
+       <label for="nombre">Nombre</label>
+       <input type="text" id="nombre" name="nombre" class="form-control">
+     </div>
+       <div class="form-group">
+        <label for="correo">Correo</label>
+       <input type="email" id="email" name="email" class="form-control">
+       </div>
+       </div>
+  <div class="col">
+        <div class="form-group">
+        <label for="telefono">Teléfono</label>
+       <input type="tel" id="telefono" name="telefono" class="form-control">
+       </div>
+       <div class="form-group">
+        <label for="password">Contraseña</label>
+       <input type="password" id="password" name="password" class="form-control">
+       </div>
+     </div>
+     </div>
+     <div class="row">
+       <div class="col">
+         <button type="button" class="btn btn-success " id="guardar_datos">Guardar</button>
+       </div>
+     </div>
+     </div>
+       </form>
     </main>
   </div>
 </div>
@@ -156,6 +165,22 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  
 <script>
+  //todas las vitas se ocultan
+  //pregunto que vista esta visible
+  //pregunto cual es la vista que se va mostrar
+  
+  function change_view(vista ='show_data'){
+    $("#main").find(".view").each(function() {
+      $(this).slideUp('fast');
+      let id=$(this).attr("id");
+      if (vista==id) {
+        $(this).slideDown(300);
+      }
+      
+    });
+
+  }
+
   function  consultar(){
     let obj = {
       "action" : "consultar_usuarios"
@@ -180,7 +205,35 @@
   }
   $(document).ready(function(){
     consultar();
+    change_view();
   }); 
+
+  $("#nuevo_registro").click(function() {
+   change_view('insert_data');
+   });
+
+  $("#guardar_datos").click(function() {
+   let obj ={
+    "action" : "insertar_usuarios"
+   }
+
+   $("#form_data").find("input").each(function(){
+    $(this).removeClass("has-error");
+   if ($(this).val() != "") {
+      obj[$(this).prop("name")] = $(this).val();
+   }else{
+    $(this).addClass("has-error").focus();
+    return false;
+   }
+  });
+
+   $.post('includes/_funciones',obj, function() {});
+
+});
+  $("#main").find(".cancelar").click(function() {
+    change_view();
+    $("#form_data")[0].reset();
+  });
 
 </script>
 </body>
